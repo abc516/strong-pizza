10193811ab09bd4e0b9f270da9efb6cbe2c629e3
Anton Cataldi <anton.cataldi@gmail.com>
2023-06-25 12:01:47 -0700

    feat: add Swagger / OpenAPI package

--- Program.cs
+++ Program.cs
@@ -3,12 +3,12 @@ using strong_pizza.Services.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<IToppingService, ToppingService>();
 builder.Services.AddScoped<IPizzaService, PizzaService>();
 
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,7 +16,15 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+} else {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();

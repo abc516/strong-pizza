5f8ec2c8e4593bb7da0c08b738db21ab54120c8c
Anton Cataldi <anton.cataldi@gmail.com>
2023-06-28 01:29:53 -0700

    fix: deployment settings config

--- Program.cs
+++ Program.cs
@@ -22,13 +22,12 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 } else {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.RoutePrefix = string.Empty;
-});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
